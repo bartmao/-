9021d3eaf2b1be105db07066b22e05496c57cf6f
bartmao <maochen.mars@qq.com>
2017-06-18 16:33:18 +0800

    Init the code

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace 公式生成工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选取公式文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(12, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(694, 457);
+            this.panel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(121, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "导出Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(230, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(176, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "当常量全为0时依然输出公式";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 510);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "公式生成工具";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace 公式生成工具
+{
+    public partial class Form1 : Form
+    {
+        private List<Parameter> parameters = new List<Parameter>();
+        private List<Formula> formulas = new List<Formula>();
+        private bool IsUpdating = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var d = new OpenFileDialog();
+            d.Filter = "文本文件|*.txt";
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                var fName = d.FileName;
+                ReadTemplate(fName);
+                RenderMyControls();
+            }
+        }
+
+        private void ReadTemplate(string fpath)
+        {
+            parameters.Clear();
+            formulas.Clear();
+            var t = File.ReadAllText(fpath);
+            var lines = t.Split('\n').Select(l => l.Trim().Replace("，", ",")).Where(l => !l.StartsWith("//"));
+
+            var flag = 1;
+            string group = string.Empty;
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    flag++;
+                    continue;
+                }
+                else if (line.StartsWith("$"))
+                {
+                    group = line.Substring(1);
+                    continue;
+                }
+
+                if (flag == 1)
+                {
+                    var pair = line.Split(',');
+                    if (!string.IsNullOrWhiteSpace(pair[0]) && !string.IsNullOrWhiteSpace(pair[1]))
+                        parameters.Add(new Parameter()
+                        {
+                            Group = group,
+                            Name = pair[0],
+                            Description = pair[1]
+                        });
+                }
+                else if (flag == 2)
+                {
+                    var equalFlag = line.IndexOf("=");
+                    if (equalFlag > -1)
+                    {
+                        string desc = string.Empty;
+                        var variable = line.Substring(0, equalFlag).Trim().Replace("（", "(");
+                        var i = variable.IndexOf('(');
+                        if (i > -1 && i < variable.Length)
+                        {
+                            desc = variable.Substring(i + 1, variable.Length - i - 2);
+                            variable = variable.Substring(0, i);
+                        }
+                        var formula = line.Substring(equalFlag + 1, line.Length - equalFlag - 1);
+                        formulas.Add(new Formula
+                        {
+                            LeftVar = variable,
+                            Description = desc,
+                            Expression = formula
+                        });
+                    }
+                }
+            }
+        }
+
+        private void RenderMyControls()
+        {
+            panel1.Controls.Clear();
+            panel1.Visible = false;
+            if (parameters.Count == 0) return;
+
+            var curGroup = new GroupBox();
+            curGroup.Text = parameters[0].Group;
+
+            var seq = 1;
+            foreach (var p in parameters)
+            {
+                if (p.Group != curGroup.Text)
+                {
+                    panel1.Controls.Add(curGroup);
+                    curGroup = new GroupBox();
+                    curGroup.Text = p.Group;
+                }
+
+                var label = new Label();
+                label.Text = p.Description;
+                label.AutoSize = false;
+                label.TextAlign = ContentAlignment.MiddleRight;
+                label.Tag = seq++;
+                var textbox = new TextBox();
+                textbox.Name = p.Name;
+                textbox.Tag = seq++;
+                curGroup.Controls.Add(label);
+                curGroup.Controls.Add(textbox);
+                textbox.BringToFront();
+            }
+
+            panel1.Controls.Add(curGroup);
+            SetControlPositions();
+        }
+
+        private void SetControlPositions()
+        {
+            panel1.Visible = false;
+            IsUpdating = true;
+            var groupTop = 20;
+            foreach (var c in panel1.Controls)
+            {
+                var group = c as GroupBox;
+                if (group != null)
+                {
+                    group.Width = panel1.Width - 20;
+                    group.Top = groupTop;
+
+                    var children = group.Controls.Cast<Control>().OrderBy(ctrl => ctrl.Tag).ToList();
+                    var curLeft = 10;
+                    var curTop = 30;
+                    for (var i = 0; i < children.Count; i += 2)
+                    {
+                        children[i].Left = curLeft;
+                        children[i].Top = curTop;
+                        children[i].Width = 100;
+                        children[i + 1].Left = curLeft + 100;
+                        children[i + 1].Top = curTop;
+                        children[i + 1].Width = 150;
+                        if (curLeft >= group.Width - 500)
+                        {
+                            curLeft = 10;
+                            curTop += 30;
+                        }
+                        else curLeft += 250;
+                    }
+                    group.Height = curTop + 30;
+                    groupTop += group.Height + 5;
+                }
+            }
+            panel1.Visible = true;
+            IsUpdating = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var variables = new Dictionary<string, string>();
+            foreach (var c in panel1.Controls.Cast<Control>())
+            {
+                foreach (var child in c.Controls.Cast<Control>())
+                {
+                    if (child is TextBox)
+                    {
+                        variables.Add(child.Name, child.Text.Trim() == string.Empty ? "0" : child.Text.Trim());
+                    }
+                }
+            }
+
+            var sb = new StringBuilder();
+            foreach (var f in formulas)
+            {
+                List<string> usedExps;
+                var fval = GetExpression(variables, f.Expression, out usedExps);
+                if (!usedExps.All(v => variables[v] == "0") || checkBox1.Checked)
+                    sb.Append(f.LeftVar + "," + f.Description + "," + fval + Environment.NewLine);
+            }
+
+            var d = new SaveFileDialog();
+            d.Filter = "表格文件（*.csv）|*.csv";
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(d.FileName, sb.ToString(), Encoding.UTF8);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("生成Excel文件失败");
+                }
+            }
+        }
+
+        private string GetExpression(Dictionary<string, string> variables, string f, out List<string> usedVariables)
+        {
+            string expression = f;
+            usedVariables = new List<string>();
+            foreach (var v in variables.Keys)
+            {
+                string replacement = variables[v];
+                if (!Regex.IsMatch(replacement, "^-?\\w+\\.?\\w*$")) replacement = "(" + replacement + ")";
+                var reg = new Regex("\\b" + v + "\\b");
+                var newExp = reg.Replace(expression, replacement);
+                if (newExp != expression) usedVariables.Add(v);
+                expression = newExp;
+            }
+            return expression;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            SetControlPositions();
+        }
+    }
+
+    public class Parameter
+    {
+        public string Group;
+        public string Name;
+        public string Description;
+    }
+
+    public class Formula
+    {
+        public string LeftVar;
+        public string Description;
+        public string Expression;
+    }
+}
